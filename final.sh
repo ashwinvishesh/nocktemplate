1115623ce84a2975a8906b04ec19c407003084e3
Ashwin Vishesh <ashwinvishesh123@gmail.com>
2025-10-09 20:32:47 +0530

    Update final.sh wallet address

--- final.sh
+++ final.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 SESSION_NAME="miner_1"
 
 # Start gpuminer in a detached screen session
-screen -dmS "$SESSION_NAME" ./gpuminer --pubkey=2ob6vm5go3gzjA14eGXkqS3x75mYL6Fz7mPAKmTbbHgytgfb2kwENnmxFbuf7xPAFTv6EdNt5rwSEpn3cwdP8XZARL7tF4FdgHc1dSqirDtrpmcS5HXszfZMMrg6fctV7Dpn --name=$(hostname) --label=Rental
+screen -dmS "$SESSION_NAME" ./gpuminer --pubkey=3k4Ytmhw79pBDcs5FvpF3S6ijag1vTSiCRraPU1dF6ADh5sgqWQueeTAnXPeb3oS56kMs9ZhVJVhXdxCtmZBkv7vyBUttFoZSz6iGAv8RK66GSGi3DR1PFnXSikzVpiRZQ8N --name=$(hostname) --label=Rental
 
 # Echo status
 echo "gpuminer is running now in screen session '$SESSION_NAME'"
