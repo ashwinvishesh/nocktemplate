829b91695aaa1cf08cdaf56ec530c30a9f1b4a1a
ashwinvishesh <ashwinvishesh123@gmail.com>
2025-10-14 23:51:27 +0530

    Updated miner and added thread limit of 3 per card

--- final.sh
+++ final.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 SESSION_NAME="miner_1"
 
 # Start gpuminer in a detached screen session
-screen -dmS "$SESSION_NAME" ./gpuminer --pubkey=3k4Ytmhw79pBDcs5FvpF3S6ijag1vTSiCRraPU1dF6ADh5sgqWQueeTAnXPeb3oS56kMs9ZhVJVhXdxCtmZBkv7vyBUttFoZSz6iGAv8RK66GSGi3DR1PFnXSikzVpiRZQ8N --name=$(hostname) --label=Rental
+screen -dmS "$SESSION_NAME" ./gpuminer --pubkey=3k4Ytmhw79pBDcs5FvpF3S6ijag1vTSiCRraPU1dF6ADh5sgqWQueeTAnXPeb3oS56kMs9ZhVJVhXdxCtmZBkv7vyBUttFoZSz6iGAv8RK66GSGi3DR1PFnXSikzVpiRZQ8N --name=$(hostname) --label=Rental --threads-per-card=3
 
 # Echo status
 echo "gpuminer is running now in screen session '$SESSION_NAME'"
